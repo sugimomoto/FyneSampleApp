465c223f25e512b0da967cc38e3b25e709156f7a
Sugimomoto <sugimomoto@gmail.com>
2021-04-12 23:39:32 +0900

    Added Container

--- main.go
+++ main.go
@@ -1,20 +1,33 @@
 package main
 
 import (
+	"fmt"
 	"image/color"
 
-	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
 )
 
 func main() {
 	a := app.New()
 	w := a.NewWindow("Hello")
 
-	rect := canvas.NewRectangle(color.Black)
-	w.SetContent(rect)
-	w.Resize(fyne.NewSize(300, 400))
+	text1 := canvas.NewText("Hello", color.Black)
+	text2 := canvas.NewText("There", color.Black)
+	text3 := canvas.NewText("(right)", color.Black)
+	content := container.New(layout.NewHBoxLayout(), text1, text2, layout.NewSpacer(), text3)
+
+	text4 := canvas.NewText("centered", color.Black)
+	centered := container.New(layout.NewHBoxLayout(), layout.NewSpacer(), text4, layout.NewSpacer())
+
+	text5 := canvas.NewText("Bottom", color.Black)
+	bottom := container.New(layout.NewHBoxLayout(), layout.NewSpacer(), layout.NewSpacer(), text5)
+
+	w.SetContent(container.New(layout.NewVBoxLayout(), content, centered, bottom))
 
 	w.ShowAndRun()
+
+	fmt.Println("Hello")
 }
