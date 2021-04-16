64b2e58aa2d36ea2025ac9141f264ef58f5a60f8
Sugimomoto <sugimomoto@gmail.com>
2021-04-16 23:37:27 +0900

    Added

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"image/color"
 
@@ -8,11 +9,9 @@ import (
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/data/binding"
 	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
-
-	"database/sql"
-
 	_ "github.com/alexbrainman/odbc"
 )
 
@@ -20,12 +19,99 @@ func main() {
 	a := app.New()
 	w := a.NewWindow("Hello")
 
-	w.SetContent(GetTableFromODBC())
+	w.SetContent(RectangleSample())
 
 	w.Resize(fyne.NewSize(300, 300))
 	w.ShowAndRun()
 
 	fmt.Println("Hello")
+}
+
+func RectangleSample() *fyne.Container {
+
+	rect1 := canvas.NewRectangle(color.White)
+	rect2 := canvas.NewRectangle(color.Black)
+	rect3 := canvas.NewRectangle(color.White)
+	rect4 := canvas.NewRectangle(color.Black)
+	rect5 := canvas.NewRectangle(color.White)
+	rect6 := canvas.NewRectangle(color.Black)
+	rect7 := canvas.NewRectangle(color.White)
+	rect8 := canvas.NewRectangle(color.Black)
+	rect9 := canvas.NewRectangle(color.White)
+
+	return container.New(layout.NewGridLayout(3),
+		rect1,
+		rect2,
+		rect3,
+		rect4,
+		rect5,
+		rect6,
+		rect7,
+		rect8,
+		rect9)
+}
+
+// 拡張Widget
+type tappableLabelWithData struct {
+	widget.Label
+}
+
+func newTappableLabelWithData(data binding.String) *tappableLabelWithData {
+	label := &tappableLabelWithData{}
+	label.Bind(data)
+	label.ExtendBaseWidget(label)
+
+	return label
+}
+
+func (t *tappableLabelWithData) Tapped(_ *fyne.PointEvent) {
+	if t.Text == "X" {
+		t.Text = "O"
+	} else {
+		t.Text = "X"
+	}
+	t.Refresh()
+}
+
+func (t *tappableLabelWithData) TappedSecondary(_ *fyne.PointEvent) {
+}
+
+func BindingSample2() *fyne.Container {
+
+	str := binding.NewString()
+	str.Set("X")
+
+	label1 := newTappableLabelWithData(str)
+
+	button2 := widget.NewButton("O", func() {})
+	button3 := widget.NewButton("O", func() {})
+	button4 := widget.NewButton("O", func() {})
+	button5 := widget.NewButton("O", func() {})
+	button6 := widget.NewButton("O", func() {})
+	button7 := widget.NewButton("O", func() {})
+	button8 := widget.NewButton("O", func() {})
+	button9 := widget.NewButton("O", func() {})
+
+	return container.New(layout.NewGridLayout(3), label1, button2, button3, button4, button5, button6, button7, button8, button9)
+}
+
+func ChangeValue() {
+
+}
+
+func BindingSample() *fyne.Container {
+
+	// 文字列をバインディング
+	str := binding.NewString()
+	str.Set("HH")
+
+	return container.NewVBox(
+		widget.NewLabelWithData(str),
+		widget.NewButton("Button", func() {
+			val, _ := str.Get()
+			str.Set(val + val)
+		}),
+	)
 }
 
 type Key struct {
